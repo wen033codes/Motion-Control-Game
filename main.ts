d99401495e30cd44222ea5e821c8873629d68ef2
wen033codes <chchwen@outlook.com>
2025-07-06 22:24:35 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
+let old_y = 0
+let old_x = 0
 let x = 2
 let y = 2
 led.plot(x, y)
 music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerUp), music.PlaybackMode.UntilDone)
 basic.forever(function () {
-	
+    old_x = x
+    old_y = y
+    if (input.acceleration(Dimension.X) > 200) {
+        x += 1
+    }
 })
